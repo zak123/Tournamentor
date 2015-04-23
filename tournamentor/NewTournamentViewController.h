a17d0dacf3fc0d9c0680fe625a158cf9e43d2093
zak123 <zacharym123@gmail.com>
2015-04-22 17:26:52 -0700

    added newTournamentViewController

---
+++ tournamentor/NewTournamentViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTournamentViewController.h
+//  
+//
+//  Created by Zachary Mallicoat on 4/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTournamentViewController : UITableViewController
+
+@end
