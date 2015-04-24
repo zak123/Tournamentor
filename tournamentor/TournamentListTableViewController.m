3fae028395088be0e5de52ccc7873eb2b4cb0c55
zak123 <zacharym123@gmail.com>
2015-04-23 21:46:16 -0700

    added cool background and fixed progress meters unloading from memery

--- tournamentor/TournamentListTableViewController.m
+++ tournamentor/TournamentListTableViewController.m
@@ -229,9 +229,30 @@
     cell.backgroundColor = [UIColor clearColor];
     int fillWidth = (progressFloat/100.0) * cell.frame.size.width;
     
-    CGRect rect = CGRectMake(0, 0, fillWidth, cell.frame.size.height);
+    if (indexPath.row & 1) {
+        cell.backgroundColor = [UIColor colorWithRed:0.267 green:0.267 blue:0.267 alpha:1];
+    }
+    else {
+        cell.backgroundColor = [UIColor colorWithRed:0.231 green:0.231 blue:0.231 alpha:1];
+    }
+    
+//    [cell setFillWidth:fillWidth];
+    
+    CGRect rect = CGRectMake(0, 70, fillWidth, cell.frame.size.height-80);
     UIView * view = [[UIView alloc] initWithFrame:rect];
-    view.backgroundColor = [UIColor greenColor];
+    
+//    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"fade"]];
+//    [cell.contentView addSubview:backgroundView];
+
+    view.backgroundColor = [UIColor colorWithRed:0.373 green:0.706 blue:0.376 alpha:1];
+    view.tag = 100;
+    
+    for (UIView *subview in cell.contentView.subviews) {
+        if (subview.tag == 100) {
+            [subview removeFromSuperview];
+        }
+    }
+    
     [cell.contentView addSubview:view];
     [cell.contentView sendSubviewToBack:view];
     
